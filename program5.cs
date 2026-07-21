d48c21fc06dab992058d8d240e3ae6599549f16a
Aditya Shejwal <adityashejwal016@gmail.com>
2026-07-21 11:35:12 +0530

    Add files via upload

---
+++ program5.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Display
+{
+    static void Main()
+    {
+        string Name = "Hello Aadi";
+        Console.WriteLine(Name);
+
+        Console.WriteLine(Name.ToUpper());
+        Console.WriteLine(Name.ToLower());
+    }
+}
