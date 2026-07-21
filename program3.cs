9a61771ebee12e3e4c04334c427f076cdf32456d
Aditya Shejwal <adityashejwal016@gmail.com>
2026-07-21 11:34:40 +0530

    Add files via upload

---
+++ program3.cs
@@ -0,0 +1,21 @@
+using System;
+
+class Display
+{
+    static void Main()
+    
+    {
+        string greeting = "     Hello World    ";
+        Console.WriteLine(greeting);
+
+        string trimmedGreeting = greeting.TrimStart();
+        Console.WriteLine(trimmedGreeting);
+
+        trimmedGreeting = greeting.TrimEnd();
+        Console.WriteLine(trimmedGreeting);
+
+        trimmedGreeting = greeting.Trim();
+        Console.WriteLine(trimmedGreeting);
+
+    }
+}
