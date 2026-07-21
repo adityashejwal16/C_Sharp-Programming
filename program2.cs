9a7137273b9dd120ff4e5b4e9a25d6251ed0584d
Aditya Shejwal <adityashejwal016@gmail.com>
2026-07-21 11:34:11 +0530

    Add files via upload

---
+++ program2.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Name
+{
+    static void Main()
+    {
+        string firstFriend = "Aadi";
+        string secoundFriend = "pooja";
+
+        Console.WriteLine(" My Firends are " + firstFriend + " and " + secoundFriend);
+
+        Console.WriteLine("The name " + firstFriend + " has " + firstFriend.Length + " letters ");
+        Console.WriteLine("The name " + secoundFriend + " has " + secoundFriend.Length + " letters ");
+    }
+}
