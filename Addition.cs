04ebe135851f5f3a5c63ba4a253754a1bc000a5e
Aditya Shejwal <adityashejwal016@gmail.com>
2026-08-01 21:02:56 +0530

    Add files via upload

---
+++ Addition.cs
@@ -0,0 +1,16 @@
+using System;
+
+class Addition
+{
+    static void Main()
+    {
+        int A = 21;
+
+        int B = 11;
+
+        int Ans = 0;
+
+        Ans = A + B;
+        Console.WriteLine("Addition is " + Ans);
+    }
+}
